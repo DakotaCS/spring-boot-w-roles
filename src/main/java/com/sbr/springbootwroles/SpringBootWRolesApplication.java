ac070f1c63be0c742c9b5ccb99453507705b4b6d
dsoares <dsoares@alinkinfo.com>
2022-04-18 13:57:40 -0400

    init

---
+++ src/main/java/com/sbr/springbootwroles/SpringBootWRolesApplication.java
@@ -0,0 +1,13 @@
+package com.sbr.springbootwroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWRolesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWRolesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbr/springbootwroles/SpringBootWRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbr.springbootwroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWRolesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
